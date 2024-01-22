49deca6bc90b167bcd6fe59e4eb51660ed2e8412
Tenné <me@wah.pink>
2024-01-22 19:18:59 +0100

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,98 +1,64 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "zohar";
   home.homeDirectory = "/var/home/zohar";
-  xdg.configHome = "/var/home/zohar/.local/share/nixbox/config";
-  xdg.dataHome = "/var/home/zohar/.local/share/nixbox/data";
-  xdg.stateHome = "/var/home/zohar/.local/share/nixbox/state";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "23.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-#  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
- # ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/zohar/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    # EDITOR = "emacs";
   };
 
   home.packages = with pkgs; [
-    direnv
-    gh
-    openssh
-    yadm
-    ripgrep
-    fd
-    nix-ld
-    fira-code
-    hasklig
-    cachix
-    gnupg
-    pinentry
-    (import (fetchTarball "https://github.com/cachix/devenv/archive/v0.6.2.tar.gz")).default
-  ];
+    (import (fetchTarball "https://github.com/cachix/devenv/archive/v0.6.3.tar.gz")).default
 
-  # Let Home Manager install and manage itself.
+    # Misc
+    cachix
+    fd
+    yadm
+
+    # Fonts
+    hasklig
+
+    # C / C++
+    clang-tools
+
+    # Python
+    black
+    pyright
+    ruff
+  ];
+  
+  fonts.fontconfig.enable = true;
+
+  programs.fish.enable = true;
+  programs.gh.enable = true;
+
+  programs.git = {
+    enable = true;
+    ignores = [
+      "/build"
+      "/build-*"
+      "/venv"
+      "/venv-*"
+    ];
+  };
+
+  programs.direnv.enable = true;
   programs.home-manager.enable = true;
 
   programs.emacs = {
     enable = true;
-    package = ((pkgs.emacsPackagesFor pkgs.emacs29-pgtk).emacsWithPackages
-      (epkgs: [ epkgs.vterm ]));
+    package = pkgs.emacs29-pgtk;
+    extraPackages = epkgs: [ epkgs.vterm ];
   };
-  programs.fish.enable = true;
-  programs.git.enable = true;
-  programs.vim.enable = true;
+
+  programs.jq.enable = true;
+  programs.lsd.enable = true;
+  programs.neovim.enable = true;
+  programs.nix-index.enable = true;
+  programs.ripgrep.enable = true;
+  programs.zellij.enable = true;
 }
